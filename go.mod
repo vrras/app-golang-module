c533e290a8a6288366028f6d66d6ca59b8050f52
Firas Luthfi <vrrases@gmail.com>
2021-01-09 00:35:15 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/vrras/app-golang-module
+
+go 1.15
+
+require (
+	github.com/vrras/golang-module-example v1.0.1
+	github.com/vrras/golang-module-example/v2 v2.0.0
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	golangmoduleexample "github.com/vrras/golang-module-example/v2"
+)
+
+func main() {
+	fmt.Println(golangmoduleexample.SayHello("Firas"))
+}
